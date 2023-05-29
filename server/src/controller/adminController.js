05233159f82e2264eddb76b2e4b3af58727aaea6
Rolandsson <102300415+Rolandsson@users.noreply.github.com>
2023-05-29 14:54:24 +0200

    outputs error code for missing book on deletion

--- server/src/controller/adminController.js
+++ server/src/controller/adminController.js
@@ -1,62 +1,28 @@
-import bookRepository from "../repository/bookRepository.js";
-import adminService from "../service/adminService.js";
 import bookService from "../service/bookService.js";
 
-const addBook = (request, response) => {
-  const {title, author, quantity} = request.body;
-  if(title == undefined || author == undefined || quantity == undefined) {
-    return response.status(403).send({error: "Missing either title, author or quantity parameter"});
-  }
-  const context = adminService.addBook(request.body);
-
-  return response.status(201).send({message: "book added successfully", context});
-}
-
-const updateBook = (request, response) => {
-  const {previous, current} = request.body;
-  if(previous === undefined || current === undefined) {
-    return response.status(403).send({error: "Missing either new book or old book"});
-  }
-
-  try {
-    const context = adminService.updateBook(previous, current);
-
-    return response.send({message: "book updated successfully", context});
-  } catch (error) {
-    return response.status(403).send({error: error.message});
-  }
-
-}
-
-const deleteBook = (request, response) => {
-  const context = adminService.deleteBook(request.body);
-
-  return response.send({message: "Book removed successfully", context})
-}
-
-const deleteUser = (request, response) => {
-  
-  try {
-    const context = adminService.deleteUser(request.body.username);
-    return response.send({message: "User: " + request.body.username + " deleted", context});
-  } catch (error) {
-    return response.status(403).send({error: error.message});
-  }
-}
-
-const getAllUsers = (request, response) => {
-  const context = adminService.viewUsers();
-
+const getAllBooks = (request, response) => {
+  const context = bookService.viewAll(response.locals.contextUUID);
   return response.send(context);
 }
 
-const addAdmin = (request, response) => {
+const searchBook = (request, response) => {
+  const context = bookService.searchBook(request.query.q);
+  return response.send(context);
+}
+
+const buyBook = (request, response) => {
+  const {title, quantity} = request.body;
+
+  if(title == undefined || quantity == undefined) {
+    return response.status(403).send({error: "Missing either title or quantity in body"});
+  }
   try {
-    const context = adminService.assignAdmin(request.body.username);
-    return response.send({message: "User: " + request.body.username + " added as administrator", context});
-  } catch (error) {
+    const context = bookService.purchaseBook(response.locals.username, title, quantity);
+
+    return response.status(200).send({message: `Purchased book ${title} successfully`, quantity, context})
+  } catch(error) {
     return response.status(403).send({error: error.message});
   }
 }
 
-export default {addBook, updateBook, deleteBook, deleteUser, getAllUsers, addAdmin};
+export default {getAllBooks, searchBook, buyBook};
